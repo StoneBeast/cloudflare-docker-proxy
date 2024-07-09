101129b78cdb992cb928d3310162023b787ba3b0
StoneBeast <72086680+StoneBeast@users.noreply.github.com>
2024-07-09 11:38:36 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.stonebeast.fun": dockerHub,
+  "quay.stonebeast.fun": "https://quay.io",
+  "gcr.stonebeast.fun": "https://gcr.io",
+  "k8s-gcr.stonebeast.fun": "https://k8s.gcr.io",
+  "k8s.stonebeast.fun": "https://registry.k8s.io",
+  "ghcr.stonebeast.fun": "https://ghcr.io",
+  "cloudsmith.stonebeast.fun": "https://docker.cloudsmith.io",
+  "ecr.stonebeast.fun": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
